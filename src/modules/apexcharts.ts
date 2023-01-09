2d94d6c727413b14263fcb51040b3b1130008601
danielduckworth <18698360+danielduckworth@users.noreply.github.com>
2023-01-09 21:00:51 +1100

    Adapt module import for ssr

--- src/modules/apexcharts.ts
+++ src/modules/apexcharts.ts
@@ -1,6 +1,17 @@
-import VueApexCharts from 'vue3-apexcharts'
+// import VueApexCharts from 'vue3-apexcharts'
+
 import { type UserModule } from '~/types'
 
-export const install: UserModule = ({ app }) => {
-  app.use(VueApexCharts)
+export const install: UserModule = ({ isClient, app }) => {
+  // app.use(VueApexCharts)
+  // Fix for ssg 'window is not defined' error. See https://github.com/antfu/vite-ssg/issues/90#issuecomment-905920343
+  if (isClient)
+    import('vue3-apexcharts').then(({ default: VueApexCharts }) => {
+      app.use(VueApexCharts)
+    },
+    import('svgmap').then(({ default: VueSvgMap }) => {
+      app.use(VueSvgMap)
+    }
+    )
+    )
 }
